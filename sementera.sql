5a0511eec85d208ac4f187a0a5bf6ce9a815e8ad
belvaaaa <148633931+belvaaaa@users.noreply.github.com>
2024-12-15 21:41:50 +0700

    Update sementera.sql

--- sementera.sql
+++ sementera.sql
@@ -1,90 +1,77 @@
--- Buat database jika belum ada dan pilih database tersebut
-CREATE DATABASE IF NOT EXISTS sementera;
-USE sementera;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- Host: 127.0.0.1
 
--- --------------------------------------------------------
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Table structure for table `admin`
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Character set settings
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Isi data untuk tabel `admin`
-INSERT IGNORE INTO `admin` (`id`, `email`, `password`) VALUES
-(3, 'demo1@gmail.com', 'demo1');
-
--- --------------------------------------------------------
-
--- Table structure for table `pekerja`
-CREATE TABLE IF NOT EXISTS `pekerja` (
-  `id_pekerja` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_pekerja` varchar(30) NOT NULL,
-  `kontak` int(12) NOT NULL,
-  PRIMARY KEY (`id_pekerja`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Isi data untuk tabel `pekerja`
-INSERT IGNORE INTO `pekerja` (`id_pekerja`, `nama_pekerja`, `kontak`) VALUES
-(42, 'ANDIKAAAA', 123),
-(43, 'ABIM', 129876),
-(44, 'BAGUS', 342121),
-(45, 'BUNGA MAWAR', 2345678),
-(46, 'CACA FARIDA', 23454321);
-
--- --------------------------------------------------------
+-- Database: `sementera`
 
 -- Table structure for table `users`
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` int(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
+CREATE TABLE `users` (
+  `id_user` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `phone` BIGINT(20) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Isi data untuk tabel `users`
-INSERT IGNORE INTO `users` (`id_user`, `name`, `email`, `phone`, `password`) VALUES
+-- Data for table `users`
+INSERT INTO `users` (`id_user`, `name`, `email`, `phone`, `password`) VALUES
 (1, 'jhofany', 'dadasd@gmail.com', 77267821, 'pbkdf2:sha256:600000$6LZRXbUA2jPVeCaZ$10ddfd87cdde871c42dc47c81be7565444df2e07edb5da8cdb13e0876dc993ef'),
-(2, 'pertemuan5', 'a23sd@gmail.com', 213, 'pbkdf2:sha256:600000$TfmYlaflTiNUTnrQ$b88df3691bdc2dc7f0f0a91a00516abcbe0f2449a51f69f520d328a52c7d4912'),
-(3, 'demo1', 'demo1@gmail.com', 21323, 'pbkdf2:sha256:600000$RkW6kY6Ff6DRwU4q$ccc509834554026b27d0587aef9375fb8bf1323f41301cc2dc4460dbcb67eac4'),
-(4, 'coba1', 'coba1@gmail.com', 2147483647, 'pbkdf2:sha256:600000$UWCukcLR2g4jjdJF$2f1bf7f3e720b420ff689655113f388632c3ba3de5aee344077204203f7dffc0'),
-(5, 'dila', 'dila@gmail.com', 8172112, 'pbkdf2:sha256:600000$c5UiPHQWZXPDc4Y0$fc17b9f06783352149588e14f818713bd26c6a18c498705560250ef1e146c4bc');
-
--- --------------------------------------------------------
+(2, 'pertemuan5', 'a23sd@gmail.com', 213, 'pbkdf2:sha256:600000$TfmYlaflTiNUTnrQ$b88df3691bdc2dc7f0f0a91a00516abcbe0f2449a51f69f520d328a52c7d4912');
 
 -- Table structure for table `pemesanan`
-CREATE TABLE IF NOT EXISTS `pemesanan` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(20) NOT NULL,
-  `nama_pemesan` varchar(20) NOT NULL,
-  `metode_pembayaran` varchar(20) NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `total_pekerja` int(12) NOT NULL,
+CREATE TABLE `pemesanan` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id_user` INT(11) NOT NULL,
+  `nama_pemesan` VARCHAR(20) NOT NULL,
+  `metode_pembayaran` VARCHAR(20) NOT NULL,
+  `status` VARCHAR(20) NOT NULL,
+  `total_pekerja` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_user` (`id_user`)
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Isi data untuk tabel `pemesanan`
-INSERT IGNORE INTO `pemesanan` (`id`, `id_user`, `nama_pemesan`, `metode_pembayaran`, `status`, `total_pekerja`) VALUES
-(14, 3, '123', '12', 'Rejected', 2),
-(15, 3, '123', '12', 'Accepted', 2),
-(16, 3, '123123', 'DANA', 'Accepted', 0),
-(17, 3, '123123', 'DANA', 'Accepted', 0),
-(18, 3, 'jhofan', 'DANA', 'pending', 0),
-(19, 3, 'dila', 'OVO', 'pending', 0),
-(20, 5, 'muse', 'GOPAY', 'pending', 0),
-(21, 7, 'firma', 'GOPAY', 'Accepted', 0),
-(22, 7, 'baron', 'OVO', 'pending', 0),
-(23, 9, 'sukirman', 'GOPAY', 'pending', 0),
-(24, 10, 'Juki', 'OVO', 'pending', 0),
-(25, 11, 'yaa', 'GOPAY', 'Accepted', 0);
+-- Data for table `pemesanan`
+INSERT INTO `pemesanan` (`id`, `id_user`, `nama_pemesan`, `metode_pembayaran`, `status`, `total_pekerja`) VALUES
+(1, 1, 'jhofany', 'DANA', 'Accepted', 2);
 
--- --------------------------------------------------------
+-- Table structure for table `admin`
+CREATE TABLE `admin` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- Data for table `admin`
+INSERT INTO `admin` (`id`, `email`, `password`) VALUES
+(1, 'admin@example.com', 'adminpass');
+
+-- Table structure for table `pekerja`
+CREATE TABLE `pekerja` (
+  `id_pekerja` INT(11) NOT NULL AUTO_INCREMENT,
+  `nama_pekerja` VARCHAR(30) NOT NULL,
+  `kontak` BIGINT(12) NOT NULL,
+  PRIMARY KEY (`id_pekerja`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data for table `pekerja`
+INSERT INTO `pekerja` (`id_pekerja`, `nama_pekerja`, `kontak`) VALUES
+(1, 'Andika', 123456789);
+
+-- End of transaction
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
