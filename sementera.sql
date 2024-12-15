14f252f1eb903e9d635d814b432db9fe6aead26c
belvaaaa <148633931+belvaaaa@users.noreply.github.com>
2024-12-15 21:47:05 +0700

    Update sementera.sql

--- sementera.sql
+++ sementera.sql
@@ -21,15 +21,15 @@ CREATE TABLE `users` (
   `id_user` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
-  `phone` BIGINT(20) NOT NULL,
+  `phone` VARCHAR(20) NOT NULL,  -- Changed to VARCHAR to store phone numbers properly
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Data for table `users`
 INSERT INTO `users` (`id_user`, `name`, `email`, `phone`, `password`) VALUES
-(1, 'jhofany', 'dadasd@gmail.com', 77267821, 'pbkdf2:sha256:600000$6LZRXbUA2jPVeCaZ$10ddfd87cdde871c42dc47c81be7565444df2e07edb5da8cdb13e0876dc993ef'),
-(2, 'pertemuan5', 'a23sd@gmail.com', 213, 'pbkdf2:sha256:600000$TfmYlaflTiNUTnrQ$b88df3691bdc2dc7f0f0a91a00516abcbe0f2449a51f69f520d328a52c7d4912');
+(1, 'jhofany', 'dadasd@gmail.com', '77267821', 'pbkdf2:sha256:600000$6LZRXbUA2jPVeCaZ$10ddfd87cdde871c42dc47c81be7565444df2e07edb5da8cdb13e0876dc993ef'),
+(2, 'pertemuan5', 'a23sd@gmail.com', '213', 'pbkdf2:sha256:600000$TfmYlaflTiNUTnrQ$b88df3691bdc2dc7f0f0a91a00516abcbe0f2449a51f69f520d328a52c7d4912');
 
 -- Table structure for table `pemesanan`
 CREATE TABLE `pemesanan` (
@@ -64,13 +64,13 @@ INSERT INTO `admin` (`id`, `email`, `password`) VALUES
 CREATE TABLE `pekerja` (
   `id_pekerja` INT(11) NOT NULL AUTO_INCREMENT,
   `nama_pekerja` VARCHAR(30) NOT NULL,
-  `kontak` BIGINT(12) NOT NULL,
+  `kontak` VARCHAR(15) NOT NULL,  -- Changed to VARCHAR to store phone numbers properly
   PRIMARY KEY (`id_pekerja`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Data for table `pekerja`
 INSERT INTO `pekerja` (`id_pekerja`, `nama_pekerja`, `kontak`) VALUES
-(1, 'Andika', 123456789);
+(1, 'Andika', '123456789');
 
 -- End of transaction
 COMMIT;
